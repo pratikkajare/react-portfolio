86f942af34a333f498d5b67828b47ec0bf58be30
pratik826 <75242181+pratik826@users.noreply.github.com>
2022-06-13 14:51:28 +0530

    link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   const form = useRef();
-  const [done, setDone] = useState(false)
+  const [done, setDone] = useState(false);
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -20,9 +20,8 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
-
           setDone(true);
-          // form.reset();
+          form.reset();
         },
         (error) => {
           console.log(error.text);
@@ -36,7 +35,7 @@ const Contact = () => {
       <div className="w-left">
         <div className="awesome">
           {/* darkMode */}
-          <span style={{ color: darkMode ? 'white' : '' }}>Get in Touch</span>
+          <span style={{ color: darkMode ? "white" : "" }}>Get in Touch</span>
           <span>Contact me</span>
           <div
             className="blur s-blur1"
@@ -47,11 +46,21 @@ const Contact = () => {
       {/* right side form */}
       <div className="c-right">
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user" placeholder="Name" />
-          <input type="email" name="user_email" className="user" placeholder="Email" />
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+          />
           <textarea name="message" className="user" placeholder="Message" />
           <input type="submit" value="Send" className="button" />
-          <span>{done && "Thanks for Contacting"}</span>
+          <span className="co_box">{done && "Thanks for Contacting"}</span>
           <div
             className="blur c-blur1"
             style={{ background: "var(--purple)" }}
